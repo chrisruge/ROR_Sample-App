59f9c3fa5869d513789f3c4613109ef1fe7faa35
chrisruge <cjruge@mtu.edu>
2019-05-28 10:44:20 -0500

    Fixed Static Pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | ROR Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | ROR Sample App"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | ROR Sample App"
+  end
+
+  test "should get contacts" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | ROR Sample App"
   end
 
 end

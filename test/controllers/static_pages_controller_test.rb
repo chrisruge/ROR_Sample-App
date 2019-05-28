cd2d73d3f1b4494ad6427c791d44ebe7f2b3a6f6
chrisruge <cjruge@mtu.edu>
2019-05-28 11:29:32 -0500

    Completed Static Pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should pull root" do
+    get '/'
+    assert_response :success
+    assert_select "title", "Home | ROR Sample App"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success

288206dfc8fb51ba5d501975fd2ec2fdd2735e09
chrisruge <cjruge@mtu.edu>
2019-05-29 15:09:22 -0500

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
-end
+
+    # Display proper title on page
+    def full_title(page_title = ' ')
+        base_title = "RoR Sample App"
+        if page_title.empty?
+            base_title
+        else
+           page_title + ' | ' + base_title
+        end
+    end
+
+end #ENDMODULE
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,31 +5,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should pull root" do
     get '/'
     assert_response :success
-    assert_select "title", "Home | ROR Sample App"
+    assert_select "title", "RoR Sample App"
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | ROR Sample App"
+    assert_select "title", "RoR Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | ROR Sample App"
+    assert_select "title", "Help | RoR Sample App"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | ROR Sample App"
+    assert_select "title", "About | RoR Sample App"
   end
 
   test "should get contacts" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact | ROR Sample App"
+    assert_select "title", "Contact | RoR Sample App"
   end
 
 end
